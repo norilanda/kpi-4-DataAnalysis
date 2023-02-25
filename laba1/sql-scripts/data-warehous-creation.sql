bb517d4c2ded5ab24d360d12a510452c9ea7554a
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2023-02-25 15:47:42 +0200

    add mysql scripts

---
+++ laba1/sql-scripts/data-warehous-creation.sql
@@ -0,0 +1,206 @@
+use disasters_dwh;
+
+drop table if exists Fact_Event;
+
+drop table if exists Dim_Location;
+
+drop table if exists Dim_Country;
+
+drop table if exists Dim_SubContinent;
+
+drop table if exists Dim_Continent;
+
+drop table if exists Dim_DamageDescription;
+
+drop table if exists Dim_Date;
+
+drop table if exists Dim_EventType;
+
+drop table if exists Dim_FlagOtherEvent;
+
+/*==============================================================*/
+/* Table: Dim_Continent                                         */
+/*==============================================================*/
+create table Dim_Continent
+(
+   ContinentID          int                            not null,
+   ContinentName        varchar(17)                    null,
+   constraint PK_DIM_CONTINENT primary key (ContinentID)
+);
+
+/*==============================================================*/
+/* Table: Dim_Country                                           */
+/*==============================================================*/
+create table Dim_Country
+(
+   CountryID            int                            not null,
+   SubContinentID       int                            null,
+   CountryName          varchar(70)                    null,
+   constraint PK_DIM_COUNTRY primary key (CountryID)
+);
+
+/*==============================================================*/
+/* Table: Dim_DamageDescription                                 */
+/*==============================================================*/
+create table Dim_DamageDescription
+(
+   DamageDescriptionID  tinyint                        not null,
+   DescriptionStatus    varchar(15)                    null,
+   LowerBound           int                            null,
+   UpperBound           int                            null,
+   constraint PK_DIM_DAMAGEDESCRIPTION primary key (DamageDescriptionID)
+);
+
+/*==============================================================*/
+/* Table: Dim_Date                                              */
+/*==============================================================*/
+create table Dim_Date
+(
+   DateID               int                            not null,
+   Year                 int                            null,
+   Month                tinyint                        null,
+   Day                  tinyint                        null,
+   constraint PK_DIM_DATE primary key (DateID)
+);
+
+/*==============================================================*/
+/* Table: Dim_EventType                                         */
+/*==============================================================*/
+create table Dim_EventType
+(
+   EventTypeID          int                            not null,
+   EventTypeName        varchar(18)                    null,
+   constraint PK_DIM_EVENTTYPE primary key (EventTypeID)
+);
+
+/*==============================================================*/
+/* Table: Dim_FlagOtherEvent                                    */
+/*==============================================================*/
+create table Dim_FlagOtherEvent
+(
+   FlagOtherEventID     int                            not null,
+   description          varchar(30)                    null,
+   constraint PK_DIM_FLAGOTHEREVENT primary key clustered (FlagOtherEventID)
+);
+
+/*==============================================================*/
+/* Table: Dim_Location                                          */
+/*==============================================================*/
+create table Dim_Location
+(
+   LocationID           int                            not null,
+   CountryID            int                            null,
+   Name                 varchar(100)                   null,
+   Lantitude            float                          null,
+   Longtitude           float                          null,
+   constraint PK_DIM_LOCATION primary key clustered (LocationID)
+);
+
+/*==============================================================*/
+/* Table: Dim_SubContinent                                      */
+/*==============================================================*/
+create table Dim_SubContinent
+(
+   SubContinentID       int                            not null,
+   ContinentID          int                            null,
+   SubContinentName     varchar(22)                    null,
+   constraint PK_DIM_SUBCONTINENT primary key clustered (SubContinentID)
+);
+
+/*==============================================================*/
+/* Table: Fact_Event                                            */
+/*==============================================================*/
+create table Fact_Event
+(
+   EventID              int                            not null,
+   LocationID           int                            null,
+   StartDateID          int                            null,
+   EndDateID            int                            null,
+   EventTypeID          int                            null,
+   FlagOtherEventID     int                            null,
+   Deaths               int                            null,
+   DeathsDescription    tinyint                        null,
+   Missing              int                            null,
+   MissingDescription   tinyint                        null,
+   Injuries             int                            null,
+   InjuriesDescription  tinyint                        null,
+   DamageMillionsDollars int                            null,
+   DamageMillionsDollarsDescription tinyint                        null,
+   HousesDamaged        int                            null,
+   HousesDamagedDescription tinyint                        null,
+   HousesDestroyed      int                            null,
+   HousesDestroyedDescription tinyint                        null,
+   TotalDeaths          int                            null,
+   TotalDeathsDescription tinyint                        null,
+   TotalMissing         int                            null,
+   TotalMissingDescription tinyint                        null,
+   TotalInjuries        int                            null,
+   TotalInjuriesDescription tinyint                        null,
+   TotalDamageMillionsDollars int                            null,
+   TotalDamageMillionsDollarsDescription tinyint                        null,
+   TotalHousesDamaged   int                            null,
+   TotalHousesDamagedDescription tinyint                        null,
+   TotalHousesDestroyed int                            null,
+   TotalHousesDestroyedDescription tinyint                        null,
+   constraint PK_FACT_EVENT primary key (EventID),
+   constraint FK_TotalHousesDestroyedDescription foreign key (TotalHousesDestroyedDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_TotalHousesDamagedDescription foreign key (TotalHousesDamagedDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_TotalDamageMillionsDollarsDescription foreign key (TotalDamageMillionsDollarsDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_TotalInjuriesDescription foreign key (TotalInjuriesDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_TotalMissingDescription foreign key (TotalMissingDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_HousesDestroyedDescription foreign key (HousesDestroyedDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_HousesDamagedDescription foreign key (HousesDamagedDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_DamageMillionsDollarsDescription foreign key (DamageMillionsDollarsDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_InjuriesDescription foreign key (InjuriesDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_MissingDescription foreign key (MissingDescription) references Dim_DamageDescription (DamageDescriptionID),
+   constraint FK_DeathsDescription foreign key (DeathsDescription) references Dim_DamageDescription (DamageDescriptionID)
+);
+
+alter table Dim_Country
+   add constraint FK_DIM_COUN_REFERENCE_DIM_SUBC foreign key (SubContinentID)
+      references Dim_SubContinent (SubContinentID)
+      on update restrict
+      on delete restrict;
+
+alter table Dim_Location
+   add constraint FK_DIM_LOCA_REFERENCE_DIM_COUN foreign key (CountryID)
+      references Dim_Country (CountryID)
+      on update restrict
+      on delete restrict;
+
+alter table Dim_SubContinent
+   add constraint FK_DIM_SUBC_REFERENCE_DIM_CONT foreign key (ContinentID)
+      references Dim_Continent (ContinentID)
+      on update restrict
+      on delete restrict;
+
+alter table Fact_Event
+   add constraint FK_FACT_EVE_REFERENCE_DIM_LOCA foreign key (LocationID)
+      references Dim_Location (LocationID)
+      on update restrict
+      on delete restrict;
+
+alter table Fact_Event
+   add constraint FK_FACT_EVE_REFERENCE_DIM_DATE foreign key (StartDateID)
+      references Dim_Date (DateID)
+      on update restrict
+      on delete restrict;
+
+alter table Fact_Event
+   add constraint FK_FACT_EVE_REFERENCE_DIM_EVEN foreign key (EventTypeID)
+      references Dim_EventType (EventTypeID)
+      on update restrict
+      on delete restrict;
+
+alter table Fact_Event
+   add constraint FK_FACT_EVE_REFERENCE_DIM_DAMA foreign key (TotalDeathsDescription)
+      references Dim_DamageDescription (DamageDescriptionID)
+      on update restrict
+      on delete restrict;
+
+alter table Fact_Event
+   add constraint FK_FACT_EVE_REFERENCE_DIM_FLAG foreign key (FlagOtherEventID)
+      references Dim_FlagOtherEvent (FlagOtherEventID)
+      on update restrict
+      on delete restrict;
+
---
+++ laba1/sql-scripts/stage-creation.sql
@@ -0,0 +1,180 @@
+use disasters_stage;
+
+drop table if exists Country;
+
+drop table if exists Earthquakes;
+
+drop table if exists TsunamiEvent;
+
+drop table if exists VolcanoEvent;
+
+/*==============================================================*/
+/* Table: Country                                               */
+/*==============================================================*/
+create table Country
+(
+   CountryID            int                            not null AUTO_INCREMENT,
+   Name                 varchar(70)                    null,
+   alpha2               varchar(3)                     null,
+   alpha3               varchar(4)                     null,
+   CountryCode          int                            null,
+   iso_3166_2           varchar(17)                    null,
+   Region               varchar(15)                    null,
+   SubRegion            varchar(30)                    null,
+   IntermediateRegion   varchar(22)                    null,
+   RegionCode           int                            null,
+   SubRegionCode        int                            null,
+   IntermidiateRegionCode int                            null,
+   constraint PK_COUNTRY primary key (CountryID)
+);
+
+/*==============================================================*/
+/* Table: Earthquakes                                           */
+/*==============================================================*/
+create table Earthquakes
+(
+   EarthquakeEventID    int                            not null AUTO_INCREMENT,
+   FlagTsunami          tinyint                        null,
+   Year                 int                            null,
+   Mo                   int                            null,
+   Dy                   int                            null,
+   Hr                   int                            null,
+   Mn                   int                            null,
+   Sec                  int                            null,
+   FocalDepth           int                            null,
+   EQ_Primary           float                          null,
+   EQ_MAG_MV            float                          null,
+   EQ_MAG_MS            float                          null,
+   EQ_MAG_MB            float                          null,
+   EQ_MAG_ML            float                          null,
+   EQ_MAG_MFA           float                          null,
+   EQ_MAG_UNK           float                          null,
+   Intensity            int                            null,
+   Country              varchar(70)                    null,
+   State                varchar(15)                    null,
+   Location             varchar(150)                   null,
+   Latitude             float                          null,
+   Longitude            float                          null,
+   RegionCode           int                            null,
+   Deaths               int                            null,
+   DeathsDescription    tinyint                        null,
+   Missing              int                            null,
+   MissingDescription   tinyint                        null,
+   Injuries             int                            null,
+   InjuriesDescription  tinyint                        null,
+   DamageMillionsDollars float                          null,
+   DamageDescription    tinyint                        null,
+   HousesDestroyed      float                          null,
+   HousesDestroyedDescription tinyint                        null,
+   HousesDamaged        float                          null,
+   HousesDamagedDescription tinyint                        null,
+   TotalDeaths          int                            null,
+   TotalDeathsDescription tinyint                        null,
+   TotalMissing         int                            null,
+   TotalMissingDescription tinyint                        null,
+   TotalInjuries        int                            null,
+   TotalInjuriesDescription tinyint                        null,
+   TotalDamage          float                          null,
+   TotalDamageDescription tinyint                        null,
+   TotalHousesDestroyed int                            null,
+   TotalHousesDestroyedDescription tinyint                        null,
+   TotalHousesDamaged   int                            null,
+   TotalHousesDamageDescription tinyint                        null,
+   constraint PK_EARTHQUAKES primary key (EarthquakeEventID)
+);
+
+/*==============================================================*/
+/* Table: TsunamiEvent                                          */
+/*==============================================================*/
+create table TsunamiEvent
+(
+   TsunamiEventID       int                            not null AUTO_INCREMENT,
+   Year                 int                            null,
+   Mo                   int                            null,
+   Dy                   int                            null,
+   Hr                   int                            null,
+   Mn                   int                            null,
+   Sec                  int                            null,
+   TsunamiEventValidity tinyint                        null,
+   TsunamiCauseCode     tinyint                        null,
+   EarthquakeMagnitude  float                          null,
+   Deposits             tinyint                        null,
+   Country              varchar(70)                    null,
+   LocationName         varchar(150)                   null,
+   Latitude             float                          null,
+   Longtitude           float                          null,
+   MaximumWaterHeightInMeters float                          null,
+   NumberOfRunups       int                            null,
+   TsunamiMagnitudeAbe  float                          null,
+   TsunamiMagnitudeIida float                          null,
+   TsunamiIntensity     float                          null,
+   Deaths               int                            null,
+   DeathsDescription    int                            null,
+   Missing              int                            null,
+   MissingDescription   int                            null,
+   Injuries             int                            null,
+   InjuriesDescription  int                            null,
+   DamageMillionsDollars int                            null,
+   DamageMillionsDollarsDescription int                            null,
+   HousesDestroyed      int                            null,
+   HousesDestroyedDescription int                            null,
+   HousesDamaged        int                            null,
+   HousesDamagedDescription int                            null,
+   TotalDeaths          int                            null,
+   TotalDeathsDescription int                            null,
+   TotalMissing         int                            null,
+   TotalMissingDescription int                            null,
+   TotalInjuries        int                            null,
+   TotalInjuriesDescription int                            null,
+   TotalDamage          int                            null,
+   TotalDamageDescriotion int                            null,
+   TotalHousesDestroyed int                            null,
+   TotalHousesDestroyedDescription int                            null,
+   TotalHousesDemaged   int                            null,
+   TotalHousesDemagedDescription int                            null,
+   constraint PK_TSUNAMIEVENT primary key (TsunamiEventID)
+);
+
+/*==============================================================*/
+/* Table: VolcanoEvent                                          */
+/*==============================================================*/
+create table VolcanoEvent
+(
+   VolcanoEventID       integer                        not null AUTO_INCREMENT,
+   Year                 int                            null,
+   Mo                   int                            null,
+   Dy                   int                            null,
+   TsunamiEvent         int                            null,
+   EarhquaqeEvent       int                            null,
+   VolcanoName          varchar(60)                    null,
+   Location             varchar(70)                    null,
+   Country              varchar(70)                    null,
+   Latitude             float                          null,
+   Longitude            float                          null,
+   Elavation            int                            null,
+   Type                 varchar(40)                    null,
+   VEI                  tinyint                        null,
+   Agent                varchar(15)                    null,
+   Deaths               int                            null,
+   DeathsDescrition     tinyint                        null,
+   Missing              int                            null,
+   MissingDescription   int                            null,
+   Injuries             int                            null,
+   InjuriesDescription  int                            null,
+   Damage               float                          null,
+   DamageDescription    tinyint                        null,
+   HousesDestroyed      int                            null,
+   HousesDestroyedDescription tinyint                        null,
+   TotalDeaths          int                            null,
+   TotalDeathsDescription tinyint                        null,
+   TotalMissing         int                            null,
+   TotalMissingDescription tinyint                        null,
+   TotalInjuries        int                            null,
+   TotalInjuriesDescription tinyint                        null,
+   TotalDamage          int                            null,
+   TotalDamageDescription tinyint                        null,
+   TotalHousesDestroed  int                            null,
+   TotalHousesDestroyedDescription tinyint                        null,
+   constraint PK_VOLCANOEVENT primary key (VolcanoEventID)
+);
+
