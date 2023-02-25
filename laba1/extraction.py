31e66accd4ac88de8a848c3d39bf22cef414fb83
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2023-02-25 21:31:52 +0200

    add earthquake extraction

--- laba1/extraction.py
+++ laba1/extraction.py
@@ -4,6 +4,7 @@ import numpy as np
 import pymysql
 from pathes import *
 from stage_area_model import *
+from config import *
 
 class Extraction:
     def __init__(self, connection: pymysql.Connection):
@@ -21,7 +22,18 @@ class Extraction:
             self.connection.commit()
 
     def earthquakes_extraction(self):
-        pass
+        data = pd.read_csv(earthquakes_file_name, delimiter=',')
+        data = data.drop('I_D', axis=1)
+        data = data.replace({np.nan: None})
+        data.replace('\t*[,]\t*', None, regex=True)
+        data = data.to_numpy()
+        columns = get_columns_without_id(self.connection, disasters_stage_db, earthquake_table)
+        columns_string = ', '.join(columns)
+
+        with self.connection.cursor() as cursor:
+            cursor.executemany(f"insert into {earthquake_table}({columns_string}) values ({self.get_format_string(len(columns))})",
+                tuple(map(tuple, data)))
+            self.connection.commit()
 
     def get_format_string(self, table_num):
         return ','.join(['%s']*table_num)
--- laba1/stage_area_model.py
+++ laba1/stage_area_model.py
@@ -1,3 +1,4 @@
+import pymysql
 country_table = 'Country'
 earthquake_table = 'Earthquakes'
 tsunami_table = 'TsunamiEvent'
@@ -7,3 +8,23 @@ country_attributes = "Name, alpha2, alpha3, CountryCode, iso_3166_2, Region, Sub
 country_column_number = 11
 earthquake_attributes = ''
 earthquake_column_number = 46
+
+def get_all_column_names(connection: pymysql.Connection, database_name: str, table_name:str):
+    cols = []
+    with connection.cursor() as cursor:
+         cursor.execute(
+             f"""SELECT `COLUMN_NAME`
+                FROM `INFORMATION_SCHEMA`.`COLUMNS`
+                WHERE `TABLE_SCHEMA`='{database_name}'
+                AND `TABLE_NAME`='{table_name}'
+                ORDER BY ordinal_position;"""
+         )
+         cols = cursor.fetchall()
+    return cols
+
+def get_columns_without_id(connection: pymysql.Connection, database_name: str, table_name:str):
+    cols_objects = get_all_column_names(connection, database_name, table_name)[1:]
+    cols = []
+    for col_obj in cols_objects:
+        cols.append(col_obj['COLUMN_NAME'])
+    return cols
