86a4d283a2f53ab6c54bfa25c87d0f31ba2d1f06
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2023-02-27 22:31:37 +0200

    insert values into evenType and flagOtherEvent tables

---
+++ laba1/sql-scripts/initial_load_insertion/insert-event-type.sql
@@ -0,0 +1,3 @@
+INSERT INTO disasters_dwh.dim_eventtype(EventTypeName) VALUES ('volcano'),
+                                                              ('earthquake'),
+                                                              ('tsunami');
---
+++ laba1/sql-scripts/initial_load_insertion/insert-flag-other-event.sql
@@ -0,0 +1,6 @@
+INSERT INTO disasters_dwh.dim_flagotherevent(description) VALUES ('volcano'),
+                                                              ('earthquake'),
+                                                              ('tsunami'),
+                                                              ('volcano, earthquake'),
+                                                              ('volcano, tsunami'),
+                                                              ('earthquake, tsunami');
